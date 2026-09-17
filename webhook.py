31b8c24213029266a94bfde4da3b644a09a80ae0
hori-1234 <m27ac49764jfgvn@t.vodafone.ne.jp>
2026-09-17 17:34:02 +0900

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -104,19 +104,3 @@ def webhook():
 @app.route("/")
 def home():
     return "Webhook server is running"
-
-# DB初期化
-@app.route("/reset-db")
-def reset_db():
-    conn = psycopg2.connect(os.environ["DATABASE_URL"])
-    cur = conn.cursor()
-
-    cur.execute("DROP TABLE IF EXISTS tickets")
-    cur.execute("DROP TABLE IF EXISTS ticket_counters")
-
-    conn.commit()
-
-    cur.close()
-    conn.close()
-
-    return "DBを初期化しました"
