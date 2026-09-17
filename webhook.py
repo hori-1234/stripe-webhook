9bb158a65b7ad59f74339ac918b090ec5e96fb75
hori-1234 <m27ac49764jfgvn@t.vodafone.ne.jp>
2026-09-17 15:49:26 +0900

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,14 +1,26 @@
 from flask import Flask, request
+import secrets
 
 app = Flask(__name__)
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
+    data = request.json
+
     print("Webhookを受信しました")
-    print(request.json)
+
+    # Stripeの購入情報
+    session = data["data"]["object"]
+
+    # ランダムなチケットIDを生成
+    ticket_id = "TKT-" + secrets.token_hex(8).upper()
+
+    print("購入金額:", session["amount_total"], "円")
+    print("決済状態:", session["payment_status"])
+    print("チケットID:", ticket_id)
+
     return "OK", 200
 
 @app.route("/")
 def home():
     return "Webhook server is running"
-
