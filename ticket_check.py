90f8deeca8a9219f1703f3e18efd39a107a62376
hori-1234 <m27ac49764jfgvn@t.vodafone.ne.jp>
2026-09-20 01:01:11 +0900

    Update ticket_check.py

--- ticket_check.py
+++ ticket_check.py
@@ -1320,6 +1320,7 @@ def cancel_ticket_send(ticket_id):
 
         resend.Emails.send({
             "from": "onboarding@resend.dev",
+            # ドメイン後修正
             "to": ["onboarding@resend.dev"],
             "subject": "チケット使用キャンセル依頼",
             "text": text
