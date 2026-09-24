7ae180f576a4e0afac26f63ec44d0909d1e252b9
hori-1234 <m27ac49764jfgvn@t.vodafone.ne.jp>
2026-09-24 19:06:24 +0900

    admin_auth.py　QRコード　サインインの表示2

--- admin_auth.py
+++ admin_auth.py
@@ -23,7 +23,7 @@ def admin_required(func):
                 401,
                 {
                     "WWW-Authenticate":
-                    'Basic realm="運営読取"'
+                    'Basic realm="unei-yomitori"'
                 }
             )
 
