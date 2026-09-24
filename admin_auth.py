a8229fff5845d12e75a603fb7a5df9f0d78132cd
hori-1234 <m27ac49764jfgvn@t.vodafone.ne.jp>
2026-09-24 19:42:37 +0900

    admin_auth.py　QRコード設定

--- admin_auth.py
+++ admin_auth.py
@@ -35,3 +35,36 @@ def admin_required(func):
         return func(*args, **kwargs)
 
     return decorated
+
+def qr_required(func):
+
+    @wraps(func)
+    def decorated(*args, **kwargs):
+
+        username = os.environ.get("QRcode_USERNAME")
+        password = os.environ.get("QRcode_PASSWORD")
+
+        auth = request.authorization
+
+        if (
+            not auth
+            or auth.username != username
+            or auth.password != password
+        ):
+            return Response(
+                """
+                <div style="font-size:32px; text-align:center; margin-top:80px;">
+                    認証がキャンセルされました。<br><br>
+                    画面を閉じてください。
+                </div>
+                """,
+                401,
+                {
+                    "WWW-Authenticate":
+                    'Basic realm="QRcode"'
+                }
+            )
+
+        return func(*args, **kwargs)
+
+    return decorated
